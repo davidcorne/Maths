d57702c07cf8e54b411cc2f1b1e643a27d8dc3f6
David Corne <davidcorne@gmail.com>
2013-04-09 14:13:18 +0100

    Add getitem in polynomials.

--- Polynomials.py
+++ Polynomials.py
@@ -11,11 +11,18 @@ class Polynomial(object):
     
     def __init__(self, polynomial_as_string, var="x"):
         """\
-        Pass in a polynomial as a string, e.g. "x^2 -2*x + 4"
+        Pass in a polynomial as a string, e.g. "x^2 - 2*x + 4"
 
         keyword argument var is the variable used for these polynomials.\
         """
-        pass
+        self.coefficients = dict()
+        poly_list = polynomial_as_string.split()
+
+    def __getitem__(self, index):
+        """\
+        This is used to get the index'th coefficient of the polynomial.\
+        """
+        return self.coefficients[index]
 
 #==============================================================================
 class utest_Polynomials(unittest.TestCase):
@@ -23,7 +30,6 @@ class utest_Polynomials(unittest.TestCase):
     def test_parser(self):
         p1 = Polynomial("x^2 - 2*x + 4")
         self.assertEqual(p1[2], 1, "Coefficent of x^1 is not 1")
-        p2 = Polynomial("x^2 - 2*x + 4")
 
 #==============================================================================
 if (__name__ == "__main__"):
