ed01111bb283a0f06a9de830594b59551a526ff4
David Corne <davidcorne@gmail.com>
2013-06-19 13:46:41 +0100

    Make Factorial.py more standard.

--- Factorial.py
+++ Factorial.py
@@ -3,16 +3,23 @@
 
 import Benchmark
 
+#==============================================================================
 @Benchmark.benchmark
-def recursive_factorial(n):
-    return __recursive_factorial(n)
+def benchmark_recursive_factorial(n):
+    return recursive_factorial(n)
 
-def __recursive_factorial(n):
+#==============================================================================
+def recursive_factorial(n):
     if (n == 1):
         return 1
-    return n * __recursive_factorial(n - 1)
+    return n * recursive_factorial(n - 1)
 
+#==============================================================================
 @Benchmark.benchmark
+def inductive_recursive_factorial(n):
+    return inductive_factorial(n)
+
+#==============================================================================
 def inductive_factorial(n):
     ret_val = 1
     for i in range(2, n + 1):
@@ -21,6 +28,6 @@ def inductive_factorial(n):
 
 #==============================================================================
 if (__name__ == "__main__"):
-    inductive_factorial(500)
-    recursive_factorial(500)
+    inductive_recursive_factorial(500)
+    benchmark_recursive_factorial(500)
     
