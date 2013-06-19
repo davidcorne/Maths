a3957a9d9323c0d770967af3103f092942dee4a7
David Corne <davidcorne@gmail.com>
2013-06-19 16:00:11 +0100

    Add more functionality to my matricies

--- matrix.py
+++ matrix.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # Written by: DGC
 
+import copy
+
 #==============================================================================
 class Matrix(object):
     
@@ -21,7 +23,7 @@ class Matrix(object):
         2 3 4
         3 4 5
 
-        Note. this is indexed from zero not one.
+        Note, this is indexed from zero not one.
         """
         self.x = x
         self.y = y
@@ -41,6 +43,15 @@ class Matrix(object):
             out.append(" ".join(row))
         return "\n".join(out)
 
+    def column(self, index):
+        return copy.copy(self[index])
+
+    def row(self, index):
+        row = []
+        for i in range(self.x):
+            row.append(self.matrix[i][index])
+        return row
+
     def __getitem__(self, key):
         """
         Returns the keyth column so that element (x, y) is accessed as 
@@ -51,10 +62,21 @@ class Matrix(object):
     def dimensions(self):
         return (self.x, self.y)
     
-    def element_wise(self, other, operation):
+    def element_wise_unary(self, operation):
+        """
+        This applies a unary function to this matrix returns a matrix of the
+        results.
+        """
+        m = Matrix(self.x, self.y)
+        for i in range(self.x):
+            for j in range(self.y):
+                m.matrix[i][j] = operation(self.matrix[i][j])
+        return m
+
+    def element_wise_binary(self, other, operation):
         """
         This applies a binary function to two matricies and returns a matrix
-        of the results
+        of the results.
         """
         if (self.dimensions() != other.dimensions()):
             raise TypeError("Matrix dimensions do not match")
@@ -64,6 +86,9 @@ class Matrix(object):
                 result = operation(self.matrix[i][j],  other.matrix[i][j])
                 m.matrix[i][j] = result
         return m
+
+    def square(self):
+        return self.x == self.y
 
     def __eq__(self, other):
         equal = False
@@ -81,7 +106,7 @@ class Matrix(object):
         return self + other
 
     def __add__(self, other):
-        return self.element_wise(other, lambda a, b: a + b)
+        return self.element_wise_binary(other, lambda a, b: a + b)
 
     def __rsub__(self, other):
         return self - other
@@ -90,31 +115,43 @@ class Matrix(object):
         return self - other
 
     def __sub__(self, other):
-        return self.element_wise(other, lambda a, b: a - b)
+        return self.element_wise_binary(other, lambda a, b: a - b)
+
+    def __imul__(self, other):
+        return self * other
+
+    def __mul__(self, other):
+        if (self.x != other.y):
+            raise TypeError("Matricies are wrong dimensions to multiply")
+        m = Matrix(other.x, self.y)
+        for i in range(other.x):
+            for j in range(self.y):
+                row = self.row(j)
+                column = other.column(i)
+                value = sum([a*b for a, b in zip(row, column)])
+                m[i][j] = value
+        return m
+        
+    def __pow__(self, power):
+        if (not self.square() and power > 1):
+            raise TypeError("Can only take powers of square matricies")
+        m = self
+        for i in range(power-1):
+            m *= self
+        return m
 
 #==============================================================================
 class SquareMatrix(Matrix):
 
     def __init__(self, size, assignment=lambda a,b: 0):
         super(SquareMatrix, self).__init__(size, size, assignment)
-        
+
 #==============================================================================
 class IdentityMatrix(SquareMatrix):
 
     def __init__(self, size):
-        super(IdentityMatrix, self).__init__(size)
-        for i in range(self.x):
-            self[i][i] = 1
+        super(IdentityMatrix, self).__init__(size, lambda a,b: int(a==b))
 
 #==============================================================================
 if (__name__ == "__main__"):
-    m = Matrix(3, 4, lambda a,b: a+b)
-    #m[0][0] = 1
-    #m[0][1] = 1
-    #m[2][2] = 1
-    i = IdentityMatrix(3)
-
-    #m.out()
-    #m += i
-    i += i
-    print m
+    pass
--- utest_Matrix.py
+++ utest_Matrix.py
@@ -17,7 +17,7 @@ class utest_Matrix(unittest.TestCase):
         expected_result = Matrix(1, 2)
         expected_result[0][0] = 5
         C = A + B
-        self.assertEqual(expected_result[0][0], C[0][0])
+        self.assertEqual(expected_result, C)
         E = Matrix(1, 1)
         self.assertRaises(TypeError, lambda : A + E)
 
@@ -52,6 +52,53 @@ class utest_Matrix(unittest.TestCase):
         for i in range(5):
             for j in range(5):
                 self.assertEqual(A[i][j], i + j)
+
+    def test_mul(self):
+        A = SquareMatrix(3, lambda a,b: a+b)
+        B = SquareMatrix(3, lambda a,b: a)
+        # 0 1 2   0 1 2    0 3 6
+        # 1 2 3 * 0 1 2 == 0 6 12
+        # 2 3 4   0 1 2    0 9 18
+        expected_result = SquareMatrix(3)
+        expected_result[1][0] = 3
+        expected_result[1][1] = 6
+        expected_result[1][2] = 9
+        expected_result[2][0] = 6
+        expected_result[2][1] = 12
+        expected_result[2][2] = 18
+        
+        self.assertEqual(A * B, expected_result)
+        self.assertNotEqual(B * A, expected_result)
+        
+        new_A = Matrix(3, 3, lambda a,b: a+b)
+        self.assertEqual(A, new_A)
+        self.assertEqual(new_A * B, expected_result)
+        
+        # cross dimension multiply
+        C = Matrix(3, 5, lambda a,b: a+b)
+        D = Matrix(1, 3, lambda a,b: b)
+        expected_result = Matrix(1, 5)
+        expected_result[0][0] = 5
+        expected_result[0][1] = 8
+        expected_result[0][2] = 11
+        expected_result[0][3] = 14
+        expected_result[0][4] = 17
+
+        self.assertEqual(C * D, expected_result)
+
+        self.assertRaises(TypeError, lambda : D * C)
+        
+        I = IdentityMatrix(3)
+        self.assertEqual(I * A, A)
+        self.assertEqual(A * I, A)
+        
+        J = IdentityMatrix(3)
+        I *= I
+        self.assertEqual(I, J)
+
+#==============================================================================
+class utest_SquareMatrix(unittest.TestCase):
+    pass
 
 #==============================================================================
 if (__name__ == "__main__"):
